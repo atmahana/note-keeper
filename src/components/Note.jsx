88bc1829532a29bc3df2e7b532b636ee5fa34f33
Adham <zubairadham.ar@gmail.com>
2023-05-16 19:29:21 +0800

    note card prop

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
-export default function Note() {
+export default function Note(props) {
   // h1 title p content
   return (
     <div className="bg-amber-100 hover:bg-amber-300 hover:drop-shadow-sm min-w-0 max-w-xs drop-shadow-md rounded-lg p-5">
-      <h1 className="font-bold">Note title</h1>
-      <p>Note Content</p>
+      <h1 className="font-bold">{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
